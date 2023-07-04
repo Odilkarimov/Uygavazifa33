dd3d4136a57190f0c10ee8c60ccc3466de17b525
odiljonswe <karimovodilbek008@gmail.com>
2023-07-04 14:41:05 +0500

    vazifa

---
+++ main.js
@@ -0,0 +1,120 @@
+class Students {
+    constructor (name , age ) {
+
+    this.name = name;
+    this.age = age; 
+    }
+
+}
+const Ali = new Students('Ali',25);
+
+class bola {
+    constructor (name , age) {
+        this.name = 'Odilbek';
+        this.age = 19;
+    }
+}
+const Odilbek = new bola ('Odilbek', 19);
+
+class Mashina {
+    constructor (name , age) {
+        this.name = 'KIA K5';
+        this.age  = 2023;
+    }
+}
+
+const KIA = new Mashina ('KIA K5', 2023);
+
+class Magazine {
+    constructor (name, age) {
+        this.name = 'Rustam';
+        this.age  = 2023;
+    }
+}
+
+const Rustam = new Magazine ('Rustam', 2023);
+
+class Dost {
+    constructor (name, age) {
+        this.name = 'Behzod';
+        this.age  = 2001;
+    }
+}
+
+const Behzod = new Dost ('Behzod', 2004);
+
+class uka {
+    constructor (name, age) {
+        this.name = 'Azizbek';
+        this.age  = 2008;
+    }
+}
+
+const Azizbek = new uka ('Azizbek', 2008);
+
+class shaxar {
+    constructor (name, age) {
+        this.name = 'Toshkent';
+        this.age  = 1943;
+    }
+}
+
+const Toshkent = new shaxar ('Toshkent', 1943);
+
+class Telefon {
+    constructor (name, age) {
+        this.name = 'Iphone 14 pro max';
+        this.age  = 2023;
+    }
+}
+
+const Iphone = new Telefon ('Iphone 14 pro max', 2023);
+
+class Konditsioner {
+    constructor (name, age) {
+        this.name = 'Samsung';
+        this.age  = 2021;
+    }
+}
+
+const Samsung = new Konditsioner ('Samsung', 2021);
+
+class kirmashina {
+    constructor (name, age) {
+        this.name = 'artel';
+        this.age  = 2019;
+    }
+}
+
+const artel = new kirmashina ('artel', 2019);
+
+class Najottalim {
+    constructor (name, age) {
+        this.name = name;
+        this.age  = age;
+    }
+}
+
+const Oquvmarkaz = new Najottalim ('Oquvmarkaz', 2018); 
+
+class scuter {
+    constructor (name, age) {
+        this.name = name;
+        this.age  = age;
+    }
+}
+
+const jet = new scuter ('jet', 2018);
+
+class shahar {
+    constructor (name, age) {
+        this.name = name;
+        this.age  = age;
+    }
+}
+
+const Maskva = new shahar ('Maskva', 1838);
+
+
+
+
